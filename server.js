b9d50c86bdb0f2514d97bbbbf49a1c652cd4b40c
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-01-13 16:44:47 +0100

    Relay messages to all clients except the sender

--- server.js
+++ server.js
@@ -4,16 +4,28 @@
 "use strict";
 var WebSocket = require("ws");
 var wss = new WebSocket.Server({ port: 29100 });
+var sockets = [];
 wss.on('connection', function (ws) {
     console.log('Connected');
+    sockets.push(ws);
     ws.on('error', function (error) {
         console.error(error);
     });
-    ws.on('message', function (message) {
-        console.log('received: %s', message);
+    ws.on('message', function (data) {
+        console.log('received: %s', data);
+        // Relay message to all clients except the sender
+        for (var _i = 0, sockets_1 = sockets; _i < sockets_1.length; _i++) {
+            var socket = sockets_1[_i];
+            if (socket != null && socket != ws) {
+                socket.send(data);
+            }
+        }
     });
     ws.on('close', function (code, message) {
         console.log("Connection closed (" + code + " " + message + ")");
+        // Remove client from list of connected clients
+        var index = sockets.indexOf(ws);
+        sockets.splice(index, 1);
     });
     ws.send('test message from the server');
 });
--- server.ts
+++ server.ts
@@ -6,17 +6,31 @@ import * as WebSocket from 'ws';
 
 let wss = new WebSocket.Server({ port: 29100 });
 
+let sockets: WebSocket[] = [];
+
 wss.on('connection', function(ws) {
   console.log('Connected');
+  sockets.push(ws);
 
   ws.on('error', function(error: Error) {
     console.error(error);
   });
-  ws.on('message', function(message) {
-    console.log('received: %s', message);
+  ws.on('message', function(data) {
+    console.log('received: %s', data);
+
+    // Relay message to all clients except the sender
+    for(let socket of sockets) {
+      if(socket != null && socket != ws) {
+        socket.send(data);
+      }
+    }
   });
   ws.on('close', function(code: number, message: string) {
     console.log(`Connection closed (${code} ${message})`);
+
+    // Remove client from list of connected clients
+    let index = sockets.indexOf(ws);
+    sockets.splice(index, 1);
   });
 
   ws.send('test message from the server');
