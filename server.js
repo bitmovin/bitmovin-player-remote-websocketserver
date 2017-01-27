eb6aab74503dfa16b12d4ea1d3a4d8881f006928
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-01-27 12:46:44 +0100

    Handle remotes and remote receivers separately and relay messages between these groups

--- server.js
+++ server.js
@@ -4,28 +4,78 @@
 "use strict";
 var WebSocket = require("ws");
 var wss = new WebSocket.Server({ port: 29100 });
-var sockets = [];
+var remoteSockets = [];
+var remoteReceiverSockets = [];
 wss.on('connection', function (ws) {
-    console.log('Connected');
-    sockets.push(ws);
+    switch (getRole(ws)) {
+        case Role.Remote:
+            console.log('Remote connected');
+            handleRemoteSocket(ws);
+            break;
+        case Role.RemoteReceiver:
+            console.log('RemoteReceiver connected');
+            handleRemoteReceiverSocket(ws);
+            break;
+        default:
+            console.error('Invalid role connected, dropping connection');
+            ws.close();
+            return;
+    }
+});
+var handleRemoteSocket = function (ws) {
+    remoteSockets.push(ws);
     ws.on('error', function (error) {
         console.error(error);
     });
     ws.on('message', function (data) {
         console.log('received: %s', data);
-        // Relay message to all clients except the sender
-        for (var _i = 0, sockets_1 = sockets; _i < sockets_1.length; _i++) {
-            var socket = sockets_1[_i];
-            if (socket != null && socket != ws) {
-                socket.send(data);
-            }
+        // Relay message from remote to all remote receivers
+        for (var _i = 0, remoteReceiverSockets_1 = remoteReceiverSockets; _i < remoteReceiverSockets_1.length; _i++) {
+            var socket = remoteReceiverSockets_1[_i];
+            socket.send(data);
         }
     });
     ws.on('close', function (code, message) {
-        console.log("Connection closed (" + code + " " + message + ")");
-        // Remove client from list of connected clients
-        var index = sockets.indexOf(ws);
-        sockets.splice(index, 1);
+        console.log("Remote connection closed (" + code + " " + message + ")");
+        // Remove remote from list of connected remotes
+        remoteItemFromArray(remoteSockets, ws);
     });
-    ws.send('test message from the server');
-});
+};
+var handleRemoteReceiverSocket = function (ws) {
+    remoteReceiverSockets.push(ws);
+    ws.on('error', function (error) {
+        console.error(error);
+    });
+    ws.on('message', function (data) {
+        console.log('received: %s', data);
+        // Relay message from receiver to all remotes
+        for (var _i = 0, remoteSockets_1 = remoteSockets; _i < remoteSockets_1.length; _i++) {
+            var socket = remoteSockets_1[_i];
+            socket.send(data);
+        }
+    });
+    ws.on('close', function (code, message) {
+        console.log("RemoteReceiver connection closed (" + code + " " + message + ")");
+        // Remove receiver from list of connected receivers
+        remoteItemFromArray(remoteReceiverSockets, ws);
+    });
+};
+var Role;
+(function (Role) {
+    Role[Role["Unknown"] = 0] = "Unknown";
+    Role[Role["Remote"] = 1] = "Remote";
+    Role[Role["RemoteReceiver"] = 2] = "RemoteReceiver";
+})(Role || (Role = {}));
+var getRole = function (ws) {
+    if (ws.upgradeReq.url.indexOf('role=receiver') === 2) {
+        return Role.RemoteReceiver;
+    }
+    else if (ws.upgradeReq.url.indexOf('role=remote') === 2) {
+        return Role.Remote;
+    }
+    return Role.Unknown;
+};
+var remoteItemFromArray = function (array, item) {
+    var index = array.indexOf(item);
+    array.splice(index, 1);
+};
--- server.ts
+++ server.ts
@@ -6,11 +6,28 @@ import * as WebSocket from 'ws';
 
 let wss = new WebSocket.Server({ port: 29100 });
 
-let sockets: WebSocket[] = [];
+let remoteSockets: WebSocket[] = [];
+let remoteReceiverSockets: WebSocket[] = [];
 
 wss.on('connection', function(ws) {
-  console.log('Connected');
-  sockets.push(ws);
+  switch (getRole(ws)) {
+    case Role.Remote:
+      console.log('Remote connected');
+      handleRemoteSocket(ws);
+      break;
+    case Role.RemoteReceiver:
+      console.log('RemoteReceiver connected');
+      handleRemoteReceiverSocket(ws);
+      break;
+    default:
+      console.error('Invalid role connected, dropping connection');
+      ws.close();
+      return;
+  }
+});
+
+let handleRemoteSocket = function(ws: WebSocket): void {
+  remoteSockets.push(ws);
 
   ws.on('error', function(error: Error) {
     console.error(error);
@@ -18,20 +35,57 @@ wss.on('connection', function(ws) {
   ws.on('message', function(data) {
     console.log('received: %s', data);
 
-    // Relay message to all clients except the sender
-    for(let socket of sockets) {
-      if(socket != null && socket != ws) {
-        socket.send(data);
-      }
+    // Relay message from remote to all remote receivers
+    for (let socket of remoteReceiverSockets) {
+      socket.send(data);
     }
   });
   ws.on('close', function(code: number, message: string) {
-    console.log(`Connection closed (${code} ${message})`);
+    console.log(`Remote connection closed (${code} ${message})`);
 
-    // Remove client from list of connected clients
-    let index = sockets.indexOf(ws);
-    sockets.splice(index, 1);
+    // Remove remote from list of connected remotes
+    remoteItemFromArray(remoteSockets, ws);
   });
+};
 
-  ws.send('test message from the server');
-});
+let handleRemoteReceiverSocket = function(ws: WebSocket): void {
+  remoteReceiverSockets.push(ws);
+
+  ws.on('error', function(error: Error) {
+    console.error(error);
+  });
+  ws.on('message', function(data) {
+    console.log('received: %s', data);
+
+    // Relay message from receiver to all remotes
+    for (let socket of remoteSockets) {
+      socket.send(data);
+    }
+  });
+  ws.on('close', function(code: number, message: string) {
+    console.log(`RemoteReceiver connection closed (${code} ${message})`);
+
+    // Remove receiver from list of connected receivers
+    remoteItemFromArray(remoteReceiverSockets, ws);
+  });
+};
+
+enum Role {
+  Unknown,
+  Remote,
+  RemoteReceiver,
+}
+
+let getRole = function(ws: WebSocket): Role {
+  if (ws.upgradeReq.url.indexOf('role=receiver') === 2) {
+    return Role.RemoteReceiver;
+  } else if (ws.upgradeReq.url.indexOf('role=remote') === 2) {
+    return Role.Remote;
+  }
+  return Role.Unknown;
+};
+
+let remoteItemFromArray = function(array: any[], item: any): void {
+  let index = array.indexOf(item);
+  array.splice(index, 1);
+};
